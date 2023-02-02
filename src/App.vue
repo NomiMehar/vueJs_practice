cd7b371094e12adecce9f14d24f04c2d3f74cd9e
NoumanDigimark <nouman@digimarkdevelopers.com>
2023-02-02 16:18:38 +0500

    vue-setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/Home/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <div class="header-wrapper">
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="50" height="50" />
+      <HelloWorld />
+    </div>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style lang="scss">
+@import './assets/main.scss';
+</style>
---
+++ src/components/Home/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template lang="">
+<div>
+    <ul class="list-none flex">
+      <li><a href="">Home</a></li>
+      <li><a href="">About</a></li>
+      <li><a href="">Company</a></li>
+      <li><a href="">Contact</a></li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  @import "./HelloWorld.scss";
+</style>
